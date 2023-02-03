aef34e9b65180b6399d502ac884d5a8d8bed9e10
Linus Eriksson <linus.piaostefan@gmail.com>
2023-02-03 10:32:37 +0100

    Skeleton is buildt

--- backendjs/app/notes/page.tsx
+++ backendjs/app/notes/page.tsx
@@ -1,10 +1,9 @@
 // import PocketBase from 'pocketbase';
 import Link from 'next/link';
 import styles from './Notes.module.css';
+import CreateNote from './CreateNote';
 
 async function getNotes() {
-  // const db = new PocketBase('http://127.0.0.1:8090');
-  // const result = await db.records.getList('notes');
   const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
   const data = await res.json();
   return data?.items as any[];
@@ -21,6 +20,7 @@ export default async function NotesPage() {
           return <Note key={note.id} note={note} />;
         })}
       </div>
+      <CreateNote />
     </div>
   );
 }
